fb8e1dc33eb767a964ab4bb6099d84861a471957
Xenoxiluna <xenoxiluna@live.com>
2020-05-07 17:26:02 -0500

    Update Dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.5"),
-        .package(url: "https://github.com/Xenoxiluna/SwiftyBytes.git", .branch("master")),
+        .package(url: "https://github.com/Xenoxiluna/SwiftyBytes.git", from: "0.1.0"),
     ],
     targets: [
         .target(name: "TerraProxy-CLI", dependencies: ["NIO", "NIOHTTP1", "Logging", "ArgumentParser", "SwiftyBytes"]),
