1f2cf5dad185c94394ab9079e19b4aca5704ca63
Xenoxiluna <xenoxiluna@live.com>
2020-06-16 20:51:43 -0500

    Update for latest version of XTerraPacket Library for testing. Also testing commands

--- README.md
+++ README.md
@@ -12,12 +12,12 @@
  ## Usage
  From terminal, run the following:
  
- `TerraProxy-CLI --proxy-target <IP or Hostname> --target-port <port> --proxy-source <IP or Hostname> --source-port <port>`
+ `TerraProxy-CLI proxy --proxy-target <IP or Hostname> --target-port <port> --proxy-source <IP or Hostname> --source-port <port>`
  
  For example:
  This will proxy your connection to the terraria server at terraserver.com on port 7777(terraserver.com:7777). Proxy source is the local ip in which the proxy server is running on. 
  
- `TerraProxy-CLI --proxy-target terraserver.com --target-port 7777 --proxy-source 127.0.0.1 --source-port 7777`
+ `TerraProxy-CLI proxy --proxy-target terraserver.com --target-port 7777 --proxy-source 127.0.0.1 --source-port 7777`
  
  When in game, you would connect to 127.0.0.1 on port 7777
  
@@ -26,7 +26,7 @@
 - [Swift-NIO v2.18.0](https://github.com/apple/swift-nio)
 - [Swift-Log 1.2.0](https://github.com/apple/swift-log)
 - [Swift-Argument-Parser 0.0.6](https://github.com/apple/swift-argument-parser)
-- [SwiftyBytes 0.2.2](https://github.com/Xenoxiluna/SwiftyBytes)
+- [SwiftyBytes 0.2.3](https://github.com/Xenoxiluna/SwiftyBytes)
 - [XTerraPacket](https://github.com/Xenoxiluna/XTerraPacket)
 
  ## Building
--- Sources/TerraProxy-CLI/main.swift
+++ Sources/TerraProxy-CLI/main.swift
@@ -10,7 +10,7 @@ import Foundation
 import ArgumentParser
 import Logging
 
-struct TerraProxy: ParsableCommand {
+struct Proxy: ParsableCommand {
     @Option(help: "IP Address of the server you are connecting to.")
     var proxyTarget: String
     
@@ -23,7 +23,7 @@ struct TerraProxy: ParsableCommand {
     @Option(help: "Port of the proxy server.")
     var sourcePort: Int
     
-    @Flag(help: "Start proxy in debug mode(NOT IMPLEMENTED)")
+    @Flag(help: "Start proxy with debug logs")
     var debugMode: Bool
 
     @Option(help: "Change logger label.")
@@ -39,6 +39,10 @@ struct TerraProxy: ParsableCommand {
         }else{
             logger = Logger(label: "com.example.TerraProxy-CLI.main")
         }
+        if debugMode{
+            logger.logLevel = .debug
+        }
+        
         let proxy  = ProxyServer(source: source, target: target, logger: logger)
 
         proxy.start { result in
@@ -49,8 +53,67 @@ struct TerraProxy: ParsableCommand {
                     logger.info("Proxy Server Started: \(source) <--> \(target)")
             }
         }
-        RunLoop.main.run()
+        
+        print("Welcome to TerraProxy!")
+        print("Type 'help' for a list of commands")
+        var shouldQuit = false
+        while !shouldQuit{
+            print("Please enter a command: ")
+            var input = getInput()
+            switch input{
+            case "start":
+                let proxy  = ProxyServer(source: source, target: target, logger: logger)
+                proxy.start { result in
+                    switch result {
+                        case .failure(let error):
+                            logger.error("Failed to start proxy server: \(error)")
+                        case .success:
+                            logger.info("Proxy Server Started: \(source) <--> \(target)")
+                    }
+                }
+            case "quit":
+                proxy.stop(then: { result in
+                    switch result {
+                        case .some(let error):
+                            logger.error("Failed to stop proxy server: \(error)")
+                        default:
+                            logger.info("Proxy Server stopped!")
+                    }
+                })
+            case "loglevel":
+                print("Not Implemented")
+                print(logger.logLevel)
+            case "help":
+                print("""
+                Command List:
+                help
+                loglevel
+                start
+                quit
+
+                """)
+            default:
+                print("Command not recognized!")
+            }
+        }
+        //RunLoop.main.run()
     }
 }
 
-TerraProxy.main()
+/*struct Info: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "A Swift command-line proxy tool for Terraria",
+        subcommands: [Proxy.self])
+
+    init() { }
+}*/
+
+func getInput() -> String {
+  let keyboard = FileHandle.standardInput
+  let inputData = keyboard.availableData
+  let strData = String(data: inputData, encoding: String.Encoding.utf8)!
+  return strData.trimmingCharacters(in: CharacterSet.newlines)
+}
+
+//Info.main()
+Proxy.main()
